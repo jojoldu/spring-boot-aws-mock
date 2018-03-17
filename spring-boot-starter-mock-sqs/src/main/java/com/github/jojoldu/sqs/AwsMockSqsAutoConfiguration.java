d1dcf5afdcb74dd576136609ddde1e9cb28dd48d
jojoldu <jojoldu@gmail.com>
2018-03-17 16:30:22 +0900

    버그수정

--- spring-boot-starter-mock-sqs/src/main/java/com/github/jojoldu/sqs/AwsMockSqsAutoConfiguration.java
+++ spring-boot-starter-mock-sqs/src/main/java/com/github/jojoldu/sqs/AwsMockSqsAutoConfiguration.java
@@ -5,11 +5,11 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.sqs.AmazonSQSAsync;
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+import com.github.jojoldu.sqs.annotation.ConditionalOnMockSqs;
 import com.github.jojoldu.sqs.config.SqsProperties;
 import com.github.jojoldu.sqs.config.SqsQueueNames;
 import org.elasticmq.rest.sqs.SQSRestServer;
 import org.elasticmq.rest.sqs.SQSRestServerBuilder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
@@ -22,7 +22,7 @@ import org.springframework.context.annotation.Primary;
  */
 
 @Configuration
-@ConditionalOnMissingBean(name = "amazonSqs")
+@ConditionalOnMockSqs
 public class AwsMockSqsAutoConfiguration {
     private SqsProperties sqsProperties;
     private SqsQueueNames sqsQueueNames;
