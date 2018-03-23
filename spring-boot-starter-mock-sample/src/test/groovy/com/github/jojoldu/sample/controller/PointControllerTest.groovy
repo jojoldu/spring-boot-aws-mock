4250575ff806d6bee78e411a28b63d2b71e73647
jojoldu <jojoldu@gmail.com>
2018-03-23 12:59:59 +0900

    테스트 큐 수신 시간 증가

--- spring-boot-starter-mock-sample/src/test/groovy/com/github/jojoldu/sample/controller/PointControllerTest.groovy
+++ spring-boot-starter-mock-sample/src/test/groovy/com/github/jojoldu/sample/controller/PointControllerTest.groovy
@@ -40,7 +40,7 @@ class PointControllerTest extends Specification {
                 .build()
         when:
         ResponseEntity<String> response = restTemplate.postForEntity("/point", requestDto, String.class)
-        Thread.sleep(500L)
+        Thread.sleep(2000L)
 
         then:
         response.getStatusCode() == HttpStatus.OK
