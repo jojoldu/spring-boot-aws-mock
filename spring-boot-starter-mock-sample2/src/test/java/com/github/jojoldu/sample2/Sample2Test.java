7cc6f186c90236c8088ded3202dd207a84b6bafc
jojoldu <jojoldu@gmail.com>
2018-03-23 13:27:05 +0900

    sample2 있으니 테스트 다 깨지네

---
+++ spring-boot-starter-mock-sample2/src/test/java/com/github/jojoldu/sample2/Sample2Test.java
@@ -0,0 +1,21 @@
+package com.github.jojoldu.sample2;
+
+import org.junit.Test;
+
+/**
+ * Created by jojoldu@gmail.com on 2018. 3. 23.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+public class Sample2Test {
+    @Test
+    public void 테스트_커버리지() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
