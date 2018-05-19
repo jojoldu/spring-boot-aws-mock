d8756acf120726622aa51107e3eb3fbd60bab0f1
jojoldu <jojoldu@gmail.com>
2018-05-19 12:37:20 +0900

    [0.2.0] 30초 타임아웃

---
+++ spring-boot-starter-mock-sample/src/test/java/com/github/jojoldu/sample/controller/Sample2ControllerTest.java
@@ -0,0 +1,69 @@
+package com.github.jojoldu.sample.controller;
+
+import com.github.jojoldu.sample.domain.Point;
+import com.github.jojoldu.sample.domain.PointRepository;
+import com.github.jojoldu.sample.dto.PointDto;
+import com.github.jojoldu.sample.listener.Sample2Listener;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by jojoldu@gmail.com on 2018. 5. 19.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Sample2ControllerTest {
+
+    @Autowired
+    Sample2Listener sample2Listener;
+
+    @Autowired
+    PointRepository pointRepository;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @After
+    public void cleanup() {
+        pointRepository.deleteAllInBatch();
+    }
+
+    @Test
+    public void Earn_points_through_the_queue() throws Exception {
+        // given
+        PointDto requestDto = PointDto.builder()
+                .userId(10L)
+                .savePoint(1000L)
+                .description("buy laptop")
+                .build();
+
+        sample2Listener.setCountDownLatch(new CountDownLatch(1));
+
+        // when
+        ResponseEntity<String> response = restTemplate.postForEntity("/sample2", requestDto, String.class);
+
+        // then
+        assertTrue(this.sample2Listener.getCountDownLatch().await(30, TimeUnit.SECONDS));
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        List<Point> points = pointRepository.findAll();
+        assertThat(points.isEmpty()).isEqualTo(false);
+        assertThat(points.get(0).getPoint()).isEqualTo(1000L);
+    }
+}
---
+++ spring-boot-starter-mock-sample/src/test/java/com/github/jojoldu/sample/controller/Sample3ControllerTest.java
@@ -0,0 +1,64 @@
+package com.github.jojoldu.sample.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.jojoldu.sample.domain.PointRepository;
+import com.github.jojoldu.sample.dto.PointDto;
+import com.github.jojoldu.sample.listener.Sample3Listener;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+/**
+ * Created by jojoldu@gmail.com on 2018. 5. 19.
+ * Blog : http://jojoldu.tistory.com
+ * Github : https://github.com/jojoldu
+ */
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class Sample3ControllerTest{
+
+    @MockBean
+    PointRepository pointRepository;
+
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    Sample3Listener pointListener;
+
+    @Test
+    public void Ack_fails_Go_to_the_dlq() throws Exception {
+        // given:
+        PointDto requestDto = PointDto.builder()
+                .userId(1L)
+                .savePoint(1000L)
+                .description("buy laptop")
+                .build();
+
+        given(pointRepository.save(any()))
+                .willThrow(new IllegalArgumentException("fail"));
+
+        pointListener.setCountDownLatch(new CountDownLatch(1));
+
+        // when:
+        restTemplate.postForEntity("/sample3", requestDto, String.class);
+
+        // then:
+        assertTrue(this.pointListener.getCountDownLatch().await(30, TimeUnit.SECONDS));
+    }
+}
