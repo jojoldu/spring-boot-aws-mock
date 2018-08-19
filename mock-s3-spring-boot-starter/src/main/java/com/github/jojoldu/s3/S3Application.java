2abbe6bb52c2402dcac8b349e276cc5c056d4f18
jojoldu <jojoldu@gmail.com>
2018-08-19 21:42:55 +0900

    s3 starter 시작

---
+++ mock-s3-spring-boot-starter/src/main/java/com/github/jojoldu/s3/S3Application.java
@@ -0,0 +1,12 @@
+package com.github.jojoldu.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(S3Application.class, args);
+    }
+}
