fea31c16045aa8916c0a545520b00f95fa5eb0ec
이동욱 <jojoldu@gmail.com>
2018-10-20 21:43:21 +0900

    redis 추가중

---
+++ mock-redis-spring-boot-starter/src/test/java/com/github/jojoldu/mockredisspringbootstarter/MockRedisSpringBootStarterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.jojoldu.mockredisspringbootstarter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MockRedisSpringBootStarterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
